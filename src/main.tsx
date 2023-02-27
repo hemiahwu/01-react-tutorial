1f6863352d2390f86a68ccd2bf55fcc893b91806
吴海洋 <why@vip.local>
2023-02-27 14:26:49 +0800

    07

--- src/main.tsx
+++ src/main.tsx
@@ -1,110 +1,39 @@
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
-// 函数组件必须有jsx/tsx返回值
-// function HelloReact() {
-//   console.log("Hello World")
-//   // 无jsx/tsx返回值,就会报错
-//   return <h2>Hello React!</h2>;
-// }
-
-// * JSX/TSX规则
-//  * 1. () 格式化 将标签用()包裹起来 与return分开距离也没关系
-// function HelloReact() {
-//   return (
-//     <div>
-//       <h1>Hello World</h1>
-//       <ul>
-//         <li>
-//           <a href="#">米斯特吴</a>
-//         </li>
-//       </ul>
-//     </div>
-//   );
-// }
-//  * 2. return 单个根标签
-// function HelloReact() {
-//   return (
-//     <div>
-//       <h1>Hello World</h1>
-//       <ul>
-//         <li><a href="#">米斯特吴</a></li>
-//       </ul>
-//     </div>
-
-//     // <div>多个根标签会报错</div>
-//   )
-// }
-//  * 3. 容器可使用div / section / article / Fragment / <></>
-// function HelloReact() {
-//   return (
-//     <>
-//       <div>
-//         <h1>Hello World</h1>
-//         <ul>
-//           <li>
-//             <a href="#">米斯特吴</a>
-//           </li>
-//         </ul>
-//       </div>
-//       <div>多个根标签会报错</div>
-//     </>
-//   );
-// }
-//  * 4. html属性名使用驼峰命名法
-// function HelloReact() {
-//   const handleClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-//     console.log("Hello React");
-//   };
-
-//   return (
-//     // onClick 驼峰
-//     <div onClick={handleClick}>
-//       <h1>Hello World</h1>
-//       <ul>
-//         <li>
-//           <a href="#">米斯特吴</a>
-//         </li>
-//       </ul>
-//     </div>
-//   );
-// }
-//  * 5. className 替换 class
-// function HelloReact() {
-//   return (
-//     // className
-//     <div className="container">
-//       <h1>Hello World</h1>
-//       <ul>
-//         <li>
-//           <a href="#">米斯特吴</a>
-//         </li>
-//       </ul>
-//     </div>
-//   );
-// }
-//  * 6. 每个标签必须有 封闭 /
-// function HelloReact() {
-//   return (
-//     <div className="container">
-//       <h1>Hello World</h1>
-//       <ul>
-//         <li>
-//           <a href="#">米斯特吴</a>
-//         </li>
-//         {/* 封闭标签 */}
-//         <img src="" alt="" />
-//       </ul>
-//     </div>
-//   );
-// }
-
-function HelloReact() {
-  return <h2>Hello React!</h2>;
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 }
 
-// ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+// Book组件
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://img10.360buyimg.com/n1/s200x200_jfs/t1/119522/15/20383/219156/61f20c1aEb50cbad5/5eeaffe8b9873f9d.jpg"
+    alt=""
+  />
+);
+
+const Title = () => <h1>我看见的未来</h1>;
+const Author = () => <h4>龙树谅</h4>;
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-root.render(<HelloReact />);
+root.render(<BookList />);
